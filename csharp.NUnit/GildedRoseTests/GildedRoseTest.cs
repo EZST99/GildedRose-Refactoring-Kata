5538cb641d68c96752f85f19336fc36f19812667
Irem <irem.arslan@gmx.at>
2025-06-22 11:11:57 +0200

    Test should reduce quality value

--- csharp.NUnit/GildedRoseTests/GildedRoseTest.cs
+++ csharp.NUnit/GildedRoseTests/GildedRoseTest.cs
@@ -16,5 +16,14 @@ namespace GildedRoseTests
             app.UpdateQuality();
             Assert.That(items[0].SellIn, Is.EqualTo(9));
         }
+
+        [Test]
+        public void Should_Reduce_Quality_Value()
+        {
+            var items = new List<Item> { new Item { Name = "foo", SellIn = 10, Quality = 5 } };
+            app = new GildedRose(items);
+            app.UpdateQuality();
+            Assert.That(items[0].Quality, Is.EqualTo(4));
+        }
     }
 }
